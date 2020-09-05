4e8b7fff1df4207016ff9d758c3219a8206679d4
Michael Giddings <mikegiddings@gmail.com>
2020-09-05 15:52:28 -0400

    added time blocks and color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,64 @@
-$(function() {});
+$(function () {});
+  
 
+/* Declare Variables */
 var today = moment().format("dddd, MMMM Do");
 
 var now = moment().format("H A");
 
-// displays the current day
+/* planWorkday entries for each hour of the workday */
+var planWorkday = [
+  { time: "9 AM", event: "" },
+  { time: "10 AM", event: "" },
+  { time: "11 AM", event: "" },
+  { time: "12 PM", event: "" },
+  { time: "1 PM", event: "" },
+  { time: "2 PM", event: "" },
+  { time: "3 PM", event: "" },
+  { time: "4 PM", event: "" },
+  { time: "5 PM", event: "" },
+];
+
+/* Local Storage check */
+var checkPrevious = JSON.parse(localStorage.getItem("dayPlanner"));
+if (checkPrevious !== null) {
+  planWorkday = checkPrevious;
+}
+
+/* Current Day */
 $("#currentDay").text(today);
 
+/* Create rows */
+planWorkday.forEach(function(timeBlock, index) {
+	var timeLabel = timeBlock.time;
+	var blockColor = colorMe(timeLabel);
+	var row =
+		'<div class="time-block" id="' +
+		index +
+		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+		timeLabel +
+		'</div><textarea class="form-control ' +
+		blockColor +
+		'">' +
+		timeBlock.event +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+
+	// show entry rows
+	$(".container").append(row);
+});
+
+/* Color rows based on current time */
+function colorMe(time) {
+	var planNow = moment(now, "H A");
+	var planEntry = moment(time, "H A");
+	if (planNow.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planNow.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
 
 console.log("Today: " + today);
 console.log("now: " + now);
